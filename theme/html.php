2c8642b4e4343b3167e1b3919ee90f635de1642b
edoceo <code@edoceo.com>
2013-08-18 22:58:38 +0000

    pretty

--- theme/html.php
+++ theme/html.php
@@ -3,9 +3,8 @@
 <head>
 <meta charset="utf-8">
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
-<meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="http://radix.edoceo.com/css/radix.css">
+<link href="http://radix.edoceo.com/css/radix.css" rel="stylesheet">
 <link href="//gcdn.org/font-awesome/3.2.1/font-awesome.css" rel="stylesheet" type="text/css">
 <link href="<?php echo radix::link('/bbd.css'); ?>" rel="stylesheet" type="text/css">
 <script src="<?php echo radix::link('/bbd.js'); ?>"></script>
@@ -13,11 +12,12 @@
 </head>
 <body>
 <header>
+<div style="float:right;line-height:32px;"><a href="http://edoceo.com/creo/bbd">BigBlueDashboard</a></div>
 <nav>
-<ul>
-<li><a href="<?php echo radix::link('/'); ?>">Dashboard</a></li>
-<li><a href="<?php echo radix::link('/status'); ?>">Status</a></li>
-<li><a href="<?php echo radix::link('/config'); ?>">Config</a></li>
+<ul class="h">
+<li><a href="<?php echo radix::link('/'); ?>"><i class="icon-dashboard"></i> Dashboard</a></li>
+<li><a href="<?php echo radix::link('/status'); ?>"><i class="icon-check"></i> Status</a></li>
+<li><a href="<?php echo radix::link('/config'); ?>"><i class="icon-cogs"></i> Config</a></li>
 </ul>
 </nav>
 </header>
