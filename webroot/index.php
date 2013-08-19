48e8e953e28ba714be749958109473efb4dba6a9
edoceo <code@edoceo.com>
2013-08-19 06:40:41 +0000

    app name nad error reporting

--- webroot/index.php
+++ webroot/index.php
@@ -5,10 +5,15 @@
 */
 
 define('APP_ROOT',dirname(dirname(__FILE__)));
+define('APP_NAME','BigBlueDashboard');
 
 require_once(APP_ROOT . '/lib/radix.php');
 require_once(APP_ROOT . '/lib/BBB.php');
 require_once(APP_ROOT . '/lib/BBB_Meeting.php');
+
+$_ENV['title'] = APP_NAME;
+
+error_reporting((E_ALL | E_STRICT) ^ E_NOTICE);
 
 radix::init();
 radix::exec();
