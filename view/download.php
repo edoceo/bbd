46481a9e02704caed1ee194865a9b7774e3f6cb7
edoceo <code@edoceo.com>
2013-08-22 05:13:08 +0000

    Fully functional targz and download

--- view/download.php
+++ view/download.php
@@ -3,22 +3,23 @@
 $mid = $_GET['m'];
 $bbm = new BBB_Meeting($mid);
 
-$path = BBB::REC_PATH . "/raw/$mid";
+$path = BBB::RAW_ARCHIVE_PATH . "/$mid";
 $file = "$path.tgz";
 
 if (!is_file($file)) {
     # $cmd = "/bin/tar --create --directory $path . |/bin/gzip >$file 2>/tmp/err.out";
     $cmd = "tar -zcf $file --directory $path . 2>&1";
     # echo $cmd;
-    # echo shell_exec($cmd);
+    shell_exec($cmd);
 }
+
+while (ob_get_level()) ob_end_clean();
 
 header('Content-Disposition: attachment; filename="Meeting_' . $bbm->code . '.tgz"');
 header('Content-Length: ' . filesize($file));
 header('Content-Transfer-Encoding: binary');
 header('Content-Type: application/octet-stream');
 
-while (ob_get_level()) ob_end_clean();
 readfile($file);
 
 exit;
