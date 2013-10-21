a569a62f85f3c8d0d3d64f5a21e4e170ce491330
edoceo <code@edoceo.com>
2013-10-21 11:31:51 -0700

    tarball, working on specific file download

--- view/download.php
+++ view/download.php
@@ -1,21 +1,47 @@
 <?php
+/**
+	@file
+	@breif Provides a Direct Download for Files in the BBB world
+*/
 
-$mid = $_GET['m'];
-$bbm = new BBB_Meeting($mid);
+$file = null;
+$name = null;
 
-$path = BBB::RAW_ARCHIVE_PATH . "/$mid";
-$file = "$path.tgz";
+// A Specific File
+if (!empty($_GET['f'])) {
 
-if (!is_file($file)) {
-    # $cmd = "/bin/tar --create --directory $path . |/bin/gzip >$file 2>/tmp/err.out";
-    $cmd = "tar -zcf $file --directory $path . 2>&1";
-    # echo $cmd;
-    shell_exec($cmd);
+	radix::dump($_GET);
+
+	$list = BBB::listPaths();
+	radix::dump($list);
+
+	exit(0);
 }
 
+// A Meeting
+if (!empty($_GET['m'])) {
+
+	// Tarball a Meeting?  I don't like this thing
+	$mid = $_GET['m'];
+	$bbm = new BBB_Meeting($mid);
+
+	$path = BBB::RAW_ARCHIVE_PATH . "/$mid";
+	$file = "$path.tgz";
+
+	if (!is_file($file)) {
+		   # $cmd = "/bin/tar --create --directory $path . |/bin/gzip >$file 2>/tmp/err.out";
+		   $cmd = "tar -zcf $file --directory $path . 2>&1";
+		   # echo $cmd;
+		   shell_exec($cmd);
+	}
+
+	$name = 'Meeting_' . $bbm->code . '.tgz';
+}
+
+// Clean Buffer
 while (ob_get_level()) ob_end_clean();
 
-header('Content-Disposition: attachment; filename="Meeting_' . $bbm->code . '.tgz"');
+header('Content-Disposition: attachment; filename="' . $name . '"');
 header('Content-Length: ' . filesize($file));
 header('Content-Transfer-Encoding: binary');
 header('Content-Type: application/octet-stream');
