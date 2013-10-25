d34fbcb9f7d1861f2b2d70928891b29796af66ca
edoceo <code@edoceo.com>
2013-10-25 13:26:34 -0700

    improve meeting query

--- controller/api/v2013.43/meeting.php
+++ controller/api/v2013.43/meeting.php
@@ -20,33 +20,22 @@ case 'POST':
 
 	break;
 case 'GET':
+
+	$res = big_meeting_list();
+
+	// Find Specific One
 	if (!empty($_GET['q'])) {
-		$res = BBB::listMeetings();
 		foreach ($res as $rec) {
-			// $xml = simplexml_load_file("/var/bigbluebutton/recording/raw/$rec/events.xml");
-			// radix::dump($xml);
-			// exit;
+			if ($rec['code'] == $_GET['q']) {
+				die(json_encode($rec));
+			}
 		}
-		die(json_encode(array(
-			'uri' => '',
-		)));
+
+		header('HTTP/1.1 404 Not Found');
+		exit(0);
 	}
 
-	// List of Meetings
-	$ret = array();
-	$res = BBB::listMeetings();
-	foreach ($res as $rec) {
-		$ret[ $rec ] = array(
-			'id' => $rec,
-		);
-		$f = "/var/bigbluebutton/recording/raw/$rec/events.xml";
-		if (is_file($f)) {
-			$xml = simplexml_load_file($f);
-			$ret[ $rec ]['code'] = strval($xml->metadata['meetingId']);
-			$ret[ $rec ]['name'] = strval($xml->metadata['meetingName']);
-		}
-	}
-	die(json_encode($ret));
+	die(json_encode($res));
 
 	break;
 }
@@ -55,3 +44,33 @@ header('HTTP/1.1 400 Bad Reqeust');
 
 exit(0);
 
+function big_meeting_list()
+{
+	// List of Meetings
+
+	$ret = array();
+	$res = BBB::listMeetings();
+	foreach ($res as $rec) {
+		$ret[ $rec ] = array(
+			'id' => $rec,
+		);
+		$f = "/var/bigbluebutton/recording/raw/$rec/events.xml";
+		if (is_file($f)) {
+			// 	$xml = simplexml_load_file($f);
+			// 	$ret[ $rec ]['code'] = strval($xml->metadata['meetingId']);
+			// 	$ret[ $rec ]['name'] = strval($xml->metadata['meetingName']);
+			if ($fh = fopen($f, 'r')) {
+				$buf = fread($fh, 256);
+				if (preg_match('/meetingId="([^"]+)"/', $buf, $m)) {
+					$ret[$rec]['code'] = $m[1];
+				}
+				if (preg_match('/meetingName="([^"]+)"/', $buf, $m)) {
+					$ret[$rec]['name'] = $m[1];
+				}
+				fclose($fh);
+			}
+		}
+	}
+
+	return $ret;
+}
