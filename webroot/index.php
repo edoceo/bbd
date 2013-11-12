7631aec5c9247fad880854a775fcfcbf8eca244c
edoceo <code@edoceo.com>
2013-11-11 18:53:52 -0800

    Use Session

--- webroot/index.php
+++ webroot/index.php
@@ -7,6 +7,8 @@
 error_reporting((E_ALL | E_STRICT) ^ E_NOTICE);
 require_once(dirname(dirname(__FILE__)) . '/boot.php');
 
+session_start();
+
 radix::init();
 if (radix::$path != '/auth') {
 	if (empty($_SESSION['uid'])) {
