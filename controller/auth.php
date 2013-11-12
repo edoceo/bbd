25cbe5cbc2b60e3de1c88fc8e179c3b4d02c7b2d
edoceo <code@edoceo.com>
2013-11-11 18:55:01 -0800

    limit to POST, redirect

--- controller/auth.php
+++ controller/auth.php
@@ -3,17 +3,20 @@
 	Auth Controller
 */
 
-$u = strtolower(trim($_POST['user']));
-
-if ($u != $_ENV['app']['user']) {
-	echo '<p class="fail">Invalid Username or Password</p>';
-	return(0);
+if (count($_POST)) {
+	$u = strtolower(trim($_POST['user']));
+	
+	if ($u != $_ENV['app']['user']) {
+		echo '<p class="fail">Invalid Username or Password</p>';
+		return(0);
+	}
+	
+	$p = trim($_POST['pass']);
+	if ($p != $_ENV['app']['pass']) {
+		echo '<p class="fail">Invalid Username or Password</p>';
+		return(0);
+	}
+	
+	$_SESSION['uid'] = $_ENV['app']['user'];
+	radix::redirect('/');
 }
-
-$p = trim($_POST['pass']);
-if ($p != $_ENV['app']['pass']) {
-	echo '<p class="fail">Invalid Username or Password</p>';
-	return(0);
-}
-
-$_SESSION['uid'] = $_ENV['app']['user'];
