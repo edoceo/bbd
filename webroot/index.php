80e0c25b19b5dc488ae00c4ae20f2a24da650d0f
edoceo <code@edoceo.com>
2013-11-15 15:58:40 -0800

    http basic for api auth

--- webroot/index.php
+++ webroot/index.php
@@ -10,8 +10,20 @@ require_once(dirname(dirname(__FILE__)) . '/boot.php');
 session_start();
 
 radix::init();
-if (radix::$path != '/auth') {
-	if (empty($_SESSION['uid'])) {
+if (preg_match('|^/api|v2013\.43|', radix::$path)) {
+	// Look for HTTP Auth
+	if ( ($_ENV['app']['user'] != $_SERVER['PHP_AUTH_USER']) || ($_ENV['app']['pass'] != $_SERVER['PHP_AUTH_PW']) ) {
+		header('HTTP/1.1 403 Forbidden', true, 403);
+		die(json_encode(array(
+			'status' => 'fail',
+			'detail' => 'Access Denied',
+		)));
+	}
+	$_SESSION['uid'] = 'api';
+}
+
+if (empty($_SESSION['uid'])) {
+	if (radix::$path != '/auth') {
 		radix::redirect('/auth');
 	}
 }
