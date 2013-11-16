080da907a8d0aaee45bfeb9e010aa108a65812a0
edoceo <code@edoceo.com>
2013-11-15 16:03:01 -0800

    use salt for auth

--- webroot/index.php
+++ webroot/index.php
@@ -11,13 +11,18 @@ session_start();
 
 radix::init();
 if (preg_match('|^/api/v2013\.43|', radix::$path)) {
-	// Look for HTTP Auth
-	if ( ($_ENV['app']['user'] != $_SERVER['PHP_AUTH_USER']) || ($_ENV['app']['pass'] != $_SERVER['PHP_AUTH_PW']) ) {
-		header('HTTP/1.1 403 Forbidden', true, 403);
-		die(json_encode(array(
-			'status' => 'fail',
-			'detail' => 'Access Denied',
-		)));
+
+	if ($_ENV['bbb']['salt'] == $_SERVER['PHP_AUTH_USER']) {
+		// OK 
+	} else {
+		// Look for HTTP Auth
+		if ( ($_ENV['app']['user'] != $_SERVER['PHP_AUTH_USER']) || ($_ENV['app']['pass'] != $_SERVER['PHP_AUTH_PW']) ) {
+			header('HTTP/1.1 403 Forbidden', true, 403);
+			die(json_encode(array(
+				'status' => 'fail',
+				'detail' => 'Access Denied',
+			)));
+		}
 	}
 	$_SESSION['uid'] = 'api';
 }
