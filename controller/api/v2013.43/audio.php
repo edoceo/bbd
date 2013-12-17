cc4d36c5eff884a5f93f23bb1e309f4eb608def2
edoceo <code@edoceo.com>
2013-12-16 18:25:18 -0800

    improve file sourcing in audio processor

--- controller/api/v2013.43/audio.php
+++ controller/api/v2013.43/audio.php
@@ -3,7 +3,7 @@
 	@file
 	@brief API for Meeting Audio
 
-	@param $_GET['q'] find a meeting with that ID
+	@param $_GET['id'] find a meeting with that ID
 
 */
 
@@ -11,11 +11,7 @@ header('Content-Type: application/json');
 
 $tmp = trim(shell_exec('mktemp -d'));
 if (!is_dir($tmp)) {
-	header('HTTP/1.1 500 Server Error', true, 500);
-	die(json_encode(array(
-		'status' => 'failure',
-		'detail' => 'Unable to create working location',
-	)));
+	exit_500('Unable to create working location');
 }
 
 // clean up handler
@@ -50,17 +46,20 @@ case 'GET':
 		case 'VOICE':
 			switch ($e['event']) {
 			case 'StartRecordingEvent':
-				$f = strval($e['source']->filename);
+				$b = basename($e['source']->filename);
+				$f = '/var/bigbluebutton/recording/raw/' . $mid . '/audio/' . $b;
 				if (!is_file($f)) {
-					$b = basename($f);
-					$f = '/var/bigbluebutton/recording/raw/' . $mid . '/audio/' . $b;
+					// The FreeSWITCH Source
+					$f = strval($e['source']->filename);
 				}
 				$audio['file'] = $f;
 				$audio['file_basename'] = basename($f);
 				$audio['time_alpha'] = $e['time_u'] - $event_alpha; // Time in ms
 
 				// Get Length from SOX
-				$buf = shell_exec('sox ' . escapeshellarg($audio['file']) . ' -n stat 2>&1');
+				$cmd = 'sox ' . escapeshellarg($audio['file']) . ' -n stat 2>&1';
+				syslog(LOG_DEBUG, $cmd);
+				$buf = shell_exec($cmd);
 				if (preg_match('/Length.+:\s+([\d\.]+)/',$buf,$m)) {
 					$audio['length_file'] = floatval($m[1]) * 1000;
 				} else {
@@ -75,27 +74,56 @@ case 'GET':
 		$event_omega = $e['time_u'];
 	}
 	$event_span = $event_omega - $event_alpha;
+	if (empty($audio['time_omega'])) {
+		$audio['time_omega'] = ($event_omega - $event_alpha);
+	}
 
 	// Generate the Audio File
-	// echo "Meeting Master Time: $meeting_duration ($meeting_omega - $meeting_alpha)\n";
-
+	// echo "\$audio['length_calc'] = {$audio['time_omega']} - {$audio['time_alpha']}\n";
 	$audio['length_calc'] = $audio['time_omega'] - $audio['time_alpha'];
 
 	// echo "Audio: {$audio['file']} at +{$audio['time_alpha']} for $audio_time\n";
 	// Prepare Audio File with Lead Time Silence "
 	// $cmd = "sox -i {$audio['file']}";
 	// shell_exec("$cmd 2>&1");
-	$audio['speed'] = $audio['length_file'] / $audio['length_calc'];
-
-	// echo 'Speed: ' .  $audio['speed'] . " file:{$audio['length_file']} / calc:{$audio['length_calc']}\n";
+	$audio['speed'] = $audio['length_file'] / $audio['length_calc']; //  / $audio['length_calc'];
 
 	// Make Leading Silence
 	sox_empty(floatval($audio['time_alpha'] / 1000), 'head.wav');
 
 	// Adjust Audio File Time and Length
-	$cmd = "sox -q -m -b 16 -c 1 -e signed -r 16000 -L -n {$audio['file']} -b 16 -c 1 -e signed -r 16000 -L -t wav body.wav speed {$audio['speed']} rate -h 16000 trim 0.000 " . floatval($audio['length_calc'] / 1000);
+	$cmd = 'sox ';
+	$cmd.= ' --buffer 131072';
+	// $cmd.= ' --multi-threaded';
+	$cmd.= ' --no-clobber';
+	$cmd.= ' --no-show-progress';
+	// $cmd.= '-m'; // --combine mix
+	// Input Options
+	$cmd.= ' --ignore-length';
+	// $cmd.= ' --bits 16'; // -b 16
+	// $cmd.= ' --channels 1'; // -c 1
+	// $cmd.= ' --encoding signed'; // -e signed
+	// $cmd.= ' --rate 16000'; // -r 16000
+	// $cmd.= ' --endian little'; // -L
+	// $cmd.= ' -n'; // What is this?
+	$cmd.= ' ' . escapeshellarg($audio['file']); // Input File
+	// Output Options
+	$cmd.= ' --bits 16';
+	$cmd.= ' --channels 1';
+	$cmd.= ' --encoding signed';
+	$cmd.= ' --rate 16000';
+	$cmd.= ' --endian little';
+	$cmd.= ' --type wav';
+	$cmd.= ' body.wav'; // Output File
+	// Effect Options
+	$cmd.= ' speed ' . sprintf('%0.16f', $audio['speed']); // See Also Stretch
+	$cmd.= ' rate -h 16000'; // high, 16k
+	$cmd.= ' trim 0.000 ' . floatval($audio['length_calc'] / 1000);
 	syslog(LOG_DEBUG, $cmd);
-	shell_exec("$cmd 2>&1");
+	$buf = shell_exec("$cmd 2>&1");
+	if (!is_file('body.wav')) {
+		exit_500("Command:$cmd\nOutput:\n$buf");
+	}
 
 	// Duration is Audio Stop Event to End of Meeting
 	sox_empty(floatval(($event_span - $audio['time_omega']) / 1000), 'tail.wav');
@@ -112,11 +140,7 @@ case 'GET':
 	$buf = shell_exec("$cmd 2>&1");
 
 	if (!is_file('work.wav')) {
-		header('HTTP/1.1 500 Sever Error', true, 500);
-		die(json_encode(array(
-			'status' => 'failure',
-			'detail' => "Command:$cmd\nOutput:\n$buf",
-		)));
+		exit_500("Command:$cmd\nOutput:\n$buf");
 	}
 
 	$buf = shell_exec('sox work.wav -n stat 2>&1');
@@ -149,13 +173,9 @@ case 'GET':
 		syslog(LOG_DEBUG, $cmd);
 		$buf = shell_exec("$cmd 2>&1");
 		if (!is_file('work.mp3')) {
-			header('HTTP/1.1 500 Sever Error', true, 500);
-			die(json_encode(array(
-				'status' => 'failure',
-				'detail' => "Command:$cmd\nOutput:\n$buf",
-			)));
+			exit_500("Command:$cmd\nOutput:\n$buf");
 		}
-		
+
 		if (is_writable("/var/bigbluebutton/published/presentation/$mid")) {
 			$file = "/var/bigbluebutton/published/presentation/$mid/audio.mp3";
 			rename('work.mp3', $file);
@@ -182,3 +202,12 @@ die(json_encode(array(
 	'status' => 'failure',
 	'detail' => 'Invalid Request',
 )));
+
+function exit_500($msg)
+{
+	header('HTTP/1.1 500 Sever Error', true, 500);
+	die(json_encode(array(
+		'status' => 'failure',
+		'detail' => $msg,
+	)));
+}
