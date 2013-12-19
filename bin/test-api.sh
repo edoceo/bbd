8c77eeaee0f5054a891b0b98738d8fbf58a3c58a
edoceo <code@edoceo.com>
2013-12-19 10:41:53 -0800

    improve test script

--- bin/test-api.sh
+++ bin/test-api.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 # salt="" meet="" ./test-api.sh
 
+# host="localhost"
+
+
+base="http://$salt@$host/bbd"
+
 # List Meetings
-echo curl -v http://$salt@localhost/bbd/api/meeting
+echo curl -v $base/api/meeting
 
 # List Live Meetings
-echo curl -v http://$salt@localhost/bbd/api/meeting?status=live
+echo curl -v $base/api/meeting?status=live
 
 # Start a Meeting
 echo curl -v -M POST \
 	--data 'name=TestMeeting' \
 	--data 'x=y' \
-	http://$salt@localhost/bbd/api/meeting
+	$base/api/meeting
 
 # Meeting Information
-echo curl -v http://$salt@localhost/bbd/api/meeting/$meet
+echo curl -v $base/api/meeting/$meet
 
 # Rebuild Meeting
 echo curl -v -M POST \
 	--data 'action=rebuild' \
-	http://$salt@localhost/bbd/api/meeting/$meet
+	$base/api/meeting/$meet
 
 # Meeting Media Information
-echo curl -v http://$salt@localhost/bbd/api/meeting/$meet/audio
+echo curl -v $base/api/meeting/$meet/audio
 
-echo curl -v http://$salt@localhost/bbd/api/meeting/$meet/video
+echo curl -v $base/api/meeting/$meet/video
 
-echo curl -v http://$salt@localhost/bbd/api/meeting/$meet/media
+echo curl -v $base/api/meeting/$meet/media
 
 # Delete the Meeting and All it's Stuff
-echo curl -v -M DELETE http://$salt@localhost/bbd/api/meeting/$meet
+echo curl -v -M DELETE $base/api/meeting/$meet
 
