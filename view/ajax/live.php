0058e1a1f3c5b683b13f8db4bac3a05a9724b08d
edoceo <code@edoceo.com>
2014-01-16 09:57:25 -0800

    control meeting

--- view/ajax/live.php
+++ view/ajax/live.php
@@ -31,35 +31,31 @@ if (!empty($res->meetings)) {
     echo '<th>Live</th>';
     echo '<th>Meeting</th>';
     echo '</tr>';
-    foreach ($res->meetings as $m) {
+    foreach ($res->meetings->meeting as $m) {
         echo '<tr>';
         $x = 'color:#f90';
-        if ('true' == strval($m->meeting->running)) $x = 'color:#0c0;';
-		echo '<td style="' . $x . '" title="Running: ' . strval($m->meeting->running) . '"><i class="fa fa-users"></i></td>';
+        if ('true' == strval($m->running)) $x = 'color:#0c0;';
+		echo '<td style="' . $x . '" title="Running: ' . strval($m->running) . '"><i class="fa fa-users"></i></td>';
 
-        echo '<td>' . strval($m->meeting->meetingID) . '/' . strval($m->meeting->meetingName) . '</td>';
-        echo '<td class="time-nice">' . strftime('%Y-%m-%d %H:%M:%S',intval($m->meeting->createTime)/1000) . '</td>';
+        echo '<td>' . strval($m->meetingID) . '/' . strval($m->meetingName) . '</td>';
+        echo '<td class="time-nice">' . strftime('%Y-%m-%d %H:%M:%S',intval($m->createTime)/1000) . '</td>';
         // date_default_timezone_set($_ENV['TZ']);
         // echo '<td>' . strftime('%Y-%m-%d %H:%M:%S',intval($m->meeting->createTime)/1000) . ' ' . $_ENV['TZ'] . '</td>';
-        if ('true' == strval($m->meeting->running)) {
-			echo '<td><button class="exec"><a href="' . radix::link('/join?m=' . $m->meeting->meetingID) . '"><i class="fa fa-sign-in"></i> Join</a></button></td>';
-			echo '<td><button class="fail"><a href="' . radix::link('/meeting/stop?m=' . $m->meeting->meetingID) . '"><i class="fa fa-exclamation"></i> Stop</a></button></td>';
+        if ('true' == strval($m->running)) {
+			echo '<td><button class="exec"><a href="' . radix::link('/join?m=' . $m->meetingID) . '"><i class="fa fa-sign-in"></i> Join</a></button></td>';
+			echo '<td><button class="fail"><a href="' . radix::link('/meeting/stop?m=' . $m->meetingID) . '"><i class="fa fa-exclamation"></i> Stop</a></button></td>';
 		}
 /*
-radix::dump($m->meeting);
+radix::dump($m);
 SimpleXMLElement Object
 (
-    [meeting] => SimpleXMLElement Object
-        (
-            [meetingID] => m339
-            [meetingName] => Meeting 339
-            [createTime] => 1376947162767
-            [attendeePW] => 123456
-            [moderatorPW] => 654321
-            [hasBeenForciblyEnded] => false
-            [running] => true
-        )
-
+	[meetingID] => m339
+	[meetingName] => Meeting 339
+	[createTime] => 1376947162767
+	[attendeePW] => 123456
+	[moderatorPW] => 654321
+	[hasBeenForciblyEnded] => false
+	[running] => true
 )
 */
         echo '</tr>';
