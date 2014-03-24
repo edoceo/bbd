85592ff172177b6269ae79ea87fd5a4de1153514
edoceo <code@edoceo.com>
2014-03-24 09:15:28 -0700

    whitespace

--- lib/BBB.php
+++ lib/BBB.php
@@ -2,95 +2,95 @@
 
 class BBB
 {
-    const BASE = '/var/bigbluebutton';
-    const STATUS = '/var/bigbluebutton/recording/status';
+	const BASE = '/var/bigbluebutton';
+	const STATUS = '/var/bigbluebutton/recording/status';
 
-    const RAW_AUDIO_SOURCE = '/var/freeswitch/meetings';
-    const RAW_VIDEO_SOURCE = '/usr/share/red5/webapps/video/streams';
-    const RAW_SHARE_SOURCE = '/var/bigbluebutton/deskshare';
-    const RAW_SLIDE_SOURCE = '/var/bigbluebutton'; // :(
-    const RAW_ARCHIVE_PATH = '/var/bigbluebutton/recording/raw';
+	const RAW_AUDIO_SOURCE = '/var/freeswitch/meetings';
+	const RAW_VIDEO_SOURCE = '/usr/share/red5/webapps/video/streams';
+	const RAW_SHARE_SOURCE = '/var/bigbluebutton/deskshare';
+	const RAW_SLIDE_SOURCE = '/var/bigbluebutton'; // :(
+	const RAW_ARCHIVE_PATH = '/var/bigbluebutton/recording/raw';
 
-    const PUBLISHED_PATH = '/var/bigbluebutton/published';
-    // const RECORDING_STATUS = '/var/bigbluebutton/recording/status';
+	const PUBLISHED_PATH = '/var/bigbluebutton/published';
+	// const RECORDING_STATUS = '/var/bigbluebutton/recording/status';
 
-    const REC_PROCESS = '/var/bigbluebutton/recording/process';
-    const REC_PUBLISH = '/var/bigbluebutton/recording/publish';
+	const REC_PROCESS = '/var/bigbluebutton/recording/process';
+	const REC_PUBLISH = '/var/bigbluebutton/recording/publish';
 
-    // /var/bigbluebutton/recording/status
-    const LOG_PATH = '/var/log/bigbluebutton';
+	// /var/bigbluebutton/recording/status
+	const LOG_PATH = '/var/log/bigbluebutton';
 
-    const CONF_BBB = '/usr/local/bigbluebutton/core/scripts/bigbluebutton.yml';
-    const CONF_TOMCAT = '/var/lib/tomcat6/webapps/bigbluebutton/WEB-INF/classes/bigbluebutton.properties';
+	const CONF_BBB = '/usr/local/bigbluebutton/core/scripts/bigbluebutton.yml';
+	const CONF_TOMCAT = '/var/lib/tomcat6/webapps/bigbluebutton/WEB-INF/classes/bigbluebutton.properties';
 
-    const CONF_FS = '/opt/freeswitch/conf';
-    const CONF_FS_ESL = '/opt/freeswitch/conf/autoload_configs/event_socket.conf.xml';
+	const CONF_FS = '/opt/freeswitch/conf';
+	const CONF_FS_ESL = '/opt/freeswitch/conf/autoload_configs/event_socket.conf.xml';
 
-    // ${SERVLET_DIR}/lti/WEB-INF/classes/lti.properties
-    //const CONF_RED5 = '/usr/share/red5/webapps/bigbluebutton/WEB-INF/red5-web.xml';
-                       // /usr/share/red5/webapps/bigbluebutton/WEB-INF/bigbluebutton.properties
-                       // /usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties
+	// ${SERVLET_DIR}/lti/WEB-INF/classes/lti.properties
+	//const CONF_RED5 = '/usr/share/red5/webapps/bigbluebutton/WEB-INF/red5-web.xml';
+					   // /usr/share/red5/webapps/bigbluebutton/WEB-INF/bigbluebutton.properties
+					   // /usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties
 
-    public static $_api_uri = null;
-    public static $_api_key = null;
+	public static $_api_uri = null;
+	public static $_api_key = null;
 
 
-    /**
-        @param $id Meeting ID
-        @param $name Friendly Name
-        @param $mpw Moderator Password
-        @param $apw Attendee PW
-        @param $rec True
+	/**
+		@param $id Meeting ID
+		@param $name Friendly Name
+		@param $mpw Moderator Password
+		@param $apw Attendee PW
+		@param $rec True
 
-        @see https://code.google.com/p/bigbluebutton/wiki/API#create
-    */
-    static function openMeeting($id,$name,$mpw,$apw,$rec=true)
-    {
-        $fn = 'create';
-        $qs = http_build_query(array(
-            'meetingID' => $id,
-            'name' => $name,
-            'moderatorPW' => $mpw,
-            'attendeePW' => $apw,
-            'record' => (!empty($rec) ? 'true' : ''),
-            // 'logouURL' => $_ENV['app']['uri_logout'],
-            // 'voiceBridge' => '000',
-        ));
+		@see https://code.google.com/p/bigbluebutton/wiki/API#create
+	*/
+	static function openMeeting($id,$name,$mpw,$apw,$rec=true)
+	{
+		$fn = 'create';
+		$qs = http_build_query(array(
+			'meetingID' => $id,
+			'name' => $name,
+			'moderatorPW' => $mpw,
+			'attendeePW' => $apw,
+			'record' => (!empty($rec) ? 'true' : ''),
+			// 'logouURL' => $_ENV['app']['uri_logout'],
+			// 'voiceBridge' => '000',
+		));
 
-        $buf = self::_api($fn,$qs);
-        $xml = simplexml_load_string($buf);
+		$buf = self::_api($fn,$qs);
+		$xml = simplexml_load_string($buf);
 
-        return $xml;
-    }
+		return $xml;
+	}
 
-    /**
-        Generates a Link to Connect to the Meeting
-        @param $id Meeting ID
-        @param $name Your Display Name
-        @param $pass Password for Joining
-    */
-    static function joinMeeting($id, $name, $pass, $args)
-    {
-        $fn = 'join';
-        $qs = array(
-            'meetingID' => $id,
-            'fullName' => $name,
-            'password' => $pass,
-        );
-        if (is_array($args)) {
-               $qs = array_merge($qs, $args);
-        }
-        ksort($qs);
-        $qs = http_build_query($qs);
-        $uri = self::_api_uri($fn,$qs);
-        return $uri;
-    }
+	/**
+		Generates a Link to Connect to the Meeting
+		@param $id Meeting ID
+		@param $name Your Display Name
+		@param $pass Password for Joining
+	*/
+	static function joinMeeting($id, $name, $pass, $args)
+	{
+		$fn = 'join';
+		$qs = array(
+			'meetingID' => $id,
+			'fullName' => $name,
+			'password' => $pass,
+		);
+		if (is_array($args)) {
+			   $qs = array_merge($qs, $args);
+		}
+		ksort($qs);
+		$qs = http_build_query($qs);
+		$uri = self::_api_uri($fn,$qs);
+		return $uri;
+	}
 
 	/**
 		Call BBB Meeting Info
 	*/
-    static function statMeeting($id, $pw)
-    {
+	static function statMeeting($id, $pw)
+	{
 		$fn = 'getMeetingInfo';
 		$qs = http_build_query(array(
 			'meetingID' => $id,
@@ -125,23 +125,23 @@ class BBB
 		return $ret;
 	}
 
-    /**
-        @param $mid MeetingId
-        @param $pw Password
-    */
-    function shutMeeting($mid,$pw)
-    {
-        $fn = 'end';
-        $qs = http_build_query(array(
-            'meetingID' => $mid,
-            'password'  => $pw,
-        ));
-        $ret = $this->_api($fn,$qs);
-        return $ret;
-    }
+	/**
+		@param $mid MeetingId
+		@param $pw Password
+	*/
+	function shutMeeting($mid,$pw)
+	{
+		$fn = 'end';
+		$qs = http_build_query(array(
+			'meetingID' => $mid,
+			'password'  => $pw,
+		));
+		$ret = $this->_api($fn,$qs);
+		return $ret;
+	}
 
-    /**
-       Get XML Configuration
+	/**
+	   Get XML Configuration
 	*/
 	static function getConfig()
 	{
@@ -174,7 +174,7 @@ class BBB
 		curl_setopt($ch, CURLOPT_POST, true);
 		// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 		// curl_setopt($ch, CURLOPT_HEADER, array(
-		//      'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
+		//	  'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
 		// ));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$ret = curl_exec($ch);
@@ -185,9 +185,9 @@ class BBB
 		return $xml;
 	}
 
-    /**
+	/**
 		List the Known Paths of BBB
-    */
+	*/
 	static function listPaths()
 	{
 	   $mirror = new ReflectionClass(__CLASS__);
@@ -198,56 +198,56 @@ class BBB
 	}
 
 	/**
-        List types of ??? Processsing?
-    */
-    static function listTypes()
-    {
-        // cd /usr/local/bigbluebutton/core/scripts/process; ls *.rb
-        return array('presentation');
-    }
+		List types of ??? Processsing?
+	*/
+	static function listTypes()
+	{
+		// cd /usr/local/bigbluebutton/core/scripts/process; ls *.rb
+		return array('presentation');
+	}
 
-    static function listMeetings($live=false)
-    {
-        if ($live) {
-            $ret = self::_api('getMeetings',null);
-            $ret = simplexml_load_string($ret);
-            return $ret;
-        }
+	static function listMeetings($live=false)
+	{
+		if ($live) {
+			$ret = self::_api('getMeetings',null);
+			$ret = simplexml_load_string($ret);
+			return $ret;
+		}
 
-        $ret = array();
-        $ml = self::_ls_meeting(self::BASE);
-        $ret = array_merge($ret,$ml);
+		$ret = array();
+		$ml = self::_ls_meeting(self::BASE);
+		$ret = array_merge($ret,$ml);
 
-        $ml = self::_ls_meeting(self::RAW_AUDIO_SOURCE);
-        $ret = array_merge($ret,$ml);
+		$ml = self::_ls_meeting(self::RAW_AUDIO_SOURCE);
+		$ret = array_merge($ret,$ml);
 
-        $ml = self::_ls_meeting(self::RAW_VIDEO_SOURCE);
-        $ret = array_merge($ret,$ml);
+		$ml = self::_ls_meeting(self::RAW_VIDEO_SOURCE);
+		$ret = array_merge($ret,$ml);
 
-        $ml = self::_ls_meeting(self::RAW_ARCHIVE_PATH);
-        $ret = array_merge($ret,$ml);
+		$ml = self::_ls_meeting(self::RAW_ARCHIVE_PATH);
+		$ret = array_merge($ret,$ml);
 
-        # ls -t /var/bigbluebutton | grep "[0-9]\{13\}$" | head -n $HEAD > $tmp_file
-        # ls -t /var/bigbluebutton/recording/raw | grep "[0-9]\{13\}$" | head -n $HEAD >> $tmp_file
-        $ret = array_unique($ret,SORT_STRING);
-        // radix::dump($ret);
+		# ls -t /var/bigbluebutton | grep "[0-9]\{13\}$" | head -n $HEAD > $tmp_file
+		# ls -t /var/bigbluebutton/recording/raw | grep "[0-9]\{13\}$" | head -n $HEAD >> $tmp_file
+		$ret = array_unique($ret,SORT_STRING);
+		// radix::dump($ret);
 
-        // Sort Newest on Top
-        usort($ret,function($a,$b) {
-            $a = intval(substr($a,-13));
-            $b = intval(substr($b,-13));
-            if ($a == $b) return 0;
-            return ($a > $b) ? -1 : 1;
-        });
+		// Sort Newest on Top
+		usort($ret,function($a,$b) {
+			$a = intval(substr($a,-13));
+			$b = intval(substr($b,-13));
+			if ($a == $b) return 0;
+			return ($a > $b) ? -1 : 1;
+		});
 
-        return $ret;
-    }
+		return $ret;
+	}
 
-    /**
+	/**
 		List Processes of BigBlueButton
-    */
-    static function listProcesses()
-    {
+	*/
+	static function listProcesses()
+	{
 		$ret = array();
 
 		$god_pid = trim(file_get_contents('/var/run/god.pid'));
@@ -297,50 +297,50 @@ class BBB
 		});
 
 		return $ret;
-    }
+	}
 
-    /**
-        @param $mid Meeting ID like 'dio1234'
-        @return false|BBB Recording XML Element
-    */
-    static function listRecordings()
-    {
-        $buf = self::_api('getRecordings',null);
-        // radix::dump($buf);
-        $xml = simplexml_load_string($buf);
-        // print_r($xml->recordings);
-        foreach ($xml->recordings->recording as $r) {
-            // print_r($r);
-            $chk = strval($r->meetingID);
-            // echo "Check1: $mid == $chk\n";
-            if ($mid == $chk) {
-                return $r;
-            }
-        }
+	/**
+		@param $mid Meeting ID like 'dio1234'
+		@return false|BBB Recording XML Element
+	*/
+	static function listRecordings()
+	{
+		$buf = self::_api('getRecordings',null);
+		// radix::dump($buf);
+		$xml = simplexml_load_string($buf);
+		// print_r($xml->recordings);
+		foreach ($xml->recordings->recording as $r) {
+			// print_r($r);
+			$chk = strval($r->meetingID);
+			// echo "Check1: $mid == $chk\n";
+			if ($mid == $chk) {
+				return $r;
+			}
+		}
 
-    }
+	}
 
-    /**
-        Internal Meeting List from Directory
-    */
-    private static function _ls_meeting($dir)
-    {
-        $ls = array();
-        $dh = opendir($dir);
-        while ($de = readdir($dh)) {
-            if (preg_match('/^([0-9a-f]+\-[0-9]+)/',$de,$m)) {
-                $ls[] = $m[1];
-            }
-        }
-        closedir($dh);
-        return $ls;
-    }
+	/**
+		Internal Meeting List from Directory
+	*/
+	private static function _ls_meeting($dir)
+	{
+		$ls = array();
+		$dh = opendir($dir);
+		while ($de = readdir($dh)) {
+			if (preg_match('/^([0-9a-f]+\-[0-9]+)/',$de,$m)) {
+				$ls[] = $m[1];
+			}
+		}
+		closedir($dh);
+		return $ls;
+	}
 
-    /**
-        Run and API Request
-    */
-    private static function _api($fn, $qs=null, $verb='GET')
-    {
+	/**
+		Run and API Request
+	*/
+	private static function _api($fn, $qs=null, $verb='GET')
+	{
 		$ch = null;
 		switch ($verb) {
 		case 'POST':
@@ -355,17 +355,17 @@ class BBB
 			$ch = curl_init(self::_api_uri($fn,$qs));
 			break;
 		}
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$ret = curl_exec($ch);
-        curl_close($ch);
-        return $ret;
-    }
+		curl_close($ch);
+		return $ret;
+	}
 
-    /**
-        Resolve the API URI
-    */
-    private static function _api_uri($fn,$qs)
-    {
+	/**
+		Resolve the API URI
+	*/
+	private static function _api_uri($fn,$qs)
+	{
 		$ret = self::$_api_uri . $fn . '?' . $qs;
 
 		if ('setConfigXML.xml' == $fn) $fn = 'setConfigXML';
@@ -373,6 +373,6 @@ class BBB
 		$ck = sha1($fn . $qs . self::$_api_key);
 		$ret.= '&checksum=' . $ck;
 		return $ret;
-    }
+	}
 
 }
